92c7c405a7bed8ab3ad1fcd8d5af98adf520f9a7
savannahgraffin <79549409+savannahgraffin@users.noreply.github.com>
2021-02-28 22:01:23 -0700

    Update branch2
    Swapped code for findFactorial() and reverseNumber() to match correct functions.

--- GitDemo-2.java
+++ GitDemo-2.java
@@ -23,24 +23,7 @@ public class GitDemo {
      *  the function will return the value of (1 * 2 * 3 * 4)
      *****/
     public double findFactorial(int num) {
-        double factorialNumber = 0;
-        
-        while( num != 0 ) {
-            double temp = num % 10;
-            factorialNumber = factorialNumber * 10 + temp;
-            num /= 10;
-        }
-        
-        return factorialNumber;
-    }
-    
-    /*****
-     * This function should reverse 'num' and return the resulting value.
-     * i.e. if num = 1234
-     *  the function will return 4321
-     *****/
-    public double reverseNumber(int num){
-        double newNumber = 1;
+         double newNumber = 1;
         
         for(int i = 1; i <= num; ++i){
             newNumber = newNumber * i;
@@ -49,4 +32,19 @@ public class GitDemo {
         return newNumber;
     }
     
+    /*****
+     * This function should reverse 'num' and return the resulting value.
+     * i.e. if num = 1234
+     *  the function will return 4321
+     *****/
+    public double reverseNumber(int num){
+           double factorialNumber = 0;
+        
+        while( num != 0 ) {
+            double temp = num % 10;
+            factorialNumber = factorialNumber * 10 + temp;
+            num /= 10;
+        }
+        
+        return factorialNumber;
 }
